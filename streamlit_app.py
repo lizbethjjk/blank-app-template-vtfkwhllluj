754117345aba5828924631c72b83639a13064fdf
lizbethjjk <lizbethjosephkoithra@gmail.com>
2024-07-06 17:40:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,27 +12,3 @@ st.set_page_config(
         }
     )
 
-
-with st.container():
-    st.title("Singapore HDB Resale Price from 2000")
-    st.image("./assets/nathan-oh-PWIOX6atM4w-unsplash.jpg")
-    st.markdown("<p style='text-align: center;'>Photo by <a href='https://unsplash.com/fr/@nathanohk' target='_blank'>Nathan Oh</a> on <a href='https://unsplash.com/photos/PWIOX6atM4w' target='_blank'>Unsplash</a></p>", unsafe_allow_html=True)
-  
-    st.markdown(
-        """
-        This dashboard is inspired by [Inside Airbnb](http://insideairbnb.com/) and various other dashboards I've come across on the web. 
-        As a new data professional, this is an ongoing project to document my learning with using Streamlit and various Python libraries 
-        to create an interactive dashboard. While this could perhaps be more easily created using PowerBI or Tableau, I am also taking the 
-        opportunity to explore the various Python plotting libraries and understand their documentation.
-
-        The project is rather close to heart since I've been looking out for a resale flat after getting married in mid-2022, although with 
-        the recent surge in resale prices as of 2022, it still remains out of reach. Hopefully this dashboard can help contribute to my 
-        eventual purchase decision, although that may also require adding in various datasets beyond the current historical information.
-
-        Data from the dashboard is retrieved from Singapore's [Data.gov.sg](https://data.gov.sg/), a free portal with access to publicly-available 
-        datasets from over 70 public agencies made available under the terms of the [Singapore Open Data License](https://data.gov.sg/open-data-licence). 
-        In particular, we dive into the HDB resale flat prices [dataset](https://data.gov.sg/dataset/resale-flat-prices), while town boundaries 
-        in the chloropeth map are retrieved from [Master Plan 2014 Planning Area Boundary](https://data.gov.sg/dataset/master-plan-2014-planning-area-boundary-no-sea).
-        """
-    )
-
