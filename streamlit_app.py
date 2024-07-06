a077de9a85a324cfadb93f5265336a9590d95fa5
lizbethjjk <lizbethjosephkoithra@gmail.com>
2024-07-06 17:38:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit as st
 import pandas as pd
-import fetch as f
 
-st.title("HDB Dataset")
-st.write(
-    "<paste intro here>"
-)
+st.set_page_config(
+    page_title="HDB Resale Price Dashboard",
+    page_icon="🏢",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        "Report a bug": "https://github.com/eeshawn11/HDB_Resale_Dashboard/issues",
+        "About": "Thanks for dropping by!"
+        }
+    )
