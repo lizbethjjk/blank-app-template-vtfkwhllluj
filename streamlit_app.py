fc58ba073de25de6ee2b4e4354803525c3b8513b
lizbethjjk <lizbethjosephkoithra@gmail.com>
2024-07-06 17:41:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,11 @@ st.set_page_config(
         }
     )
 
+@st.cache_data(show_spinner=False, max_entries=1, ttl=2_630_000)  # dataset is updated monthly
+def load_data():
+    df_web = f.get_data()
+    df_stored = pd.read_parquet("./assets/dataset.parquet")
+    df = pd.concat([df_web, df_stored], axis=0)
+    hdb_coordinates = f.get_coords_df()
+    geo_df = f.get_chloropeth()
+    return df, hdb_coordinates, geo_df
